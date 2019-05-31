ac53a09be8108bbd0b6d00b2aef5762208345930
Maxence <maxencek7@gmail.com>
2019-05-31 14:41:56 -0400

    Fixing a typo

--- Source/Build.cpp
+++ Source/Build.cpp
@@ -7,7 +7,7 @@ namespace Tools
 #ifdef __linux__
 	void Build(std::string asmfile, std::string out)
 	{
-		std::cout << "Starting build scripts on windows" << std::endl;
+		std::cout << "Starting build scripts on linux" << std::endl;
 		std::string cmd("nasm -f win32 ");
 		cmd.append(asmfile).append(" -o bin.o");
 		std::string cmdB = "ld bin.o -o ";
