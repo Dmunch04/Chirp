5bef52e4670ca4deb6d8ebf26d7f2ff026d0ece3
Maxence <maxencek7@gmail.com>
2019-06-01 11:43:54 -0400

    Pretty much only for travis

--- Tools/Build/build.sh
+++
@@ -1,1 +0,0 @@
-g++ -I ../../Source/ -I ../../Source/Console -I ../../Source/Logger -I ../../Source/Parser ../../Source/*.cpp ../../Source/Console/*.cpp ../../Source/Logger/*.cpp ../../Source/Parser/*.cpp -std=c++11 -o Chirp.o
