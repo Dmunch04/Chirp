eb55bbecc1e088ecaeaa79ce846edacd108cd78e
Maxence Cossette <maxencek7@gmail.com>
2019-06-01 11:02:13 -0400

    *try* to setup travis-ci

---
+++ Tools/Build/build.sh
@@ -0,0 +1,1 @@
+g++ -I ../../Source/ -I ../../Source/Console -I ../../Source/Logger -I ../../Source/Parser ../../Source/*.cpp ../../Source/Console/*.cpp ../../Source/Logger/*.cpp ../../Source/Parser/*.cpp -std=c++11 -o Chirp.o
