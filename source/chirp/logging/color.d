c52f62a9b5e85403eae6344c6103c8bba14f7afd
Daniel Munch <daniellmunch@gmail.com>
2020-05-25 14:32:24 +0200

    Fixed bold color codes

--- source/chirp/logging/color.d
+++ source/chirp/logging/color.d
@@ -13,21 +13,21 @@ enum Color : string
     reversed = "\u001b[7m",
 
     black = "\u001b[30m",
-    boldBlack = "\u001b[30m;1m",
+    boldBlack = "\u001b[30;1m",
     red = "\u001b[31m",
-    boldRed = "\u001b[31m;1m",
+    boldRed = "\u001b[31;1m",
     green = "\u001b[32m",
-    boldGreen = "\u001b[32m;1m",
+    boldGreen = "\u001b[32;1m",
     yellow = "\u001b[33m",
-    boldYellow = "\u001b[33m;1m",
+    boldYellow = "\u001b[33;1m",
     blue = "\u001b[34m",
-    boldBlue = "\u001b[34m;1m",
+    boldBlue = "\u001b[34;1m",
     purple = "\u001b[35m",
-    boldPurple = "\u001b[35m;1m",
+    boldPurple = "\u001b[35;1m",
     cyan = "\u001b[36m",
-    boldCyan = "\u001b[36m;1m",
+    boldCyan = "\u001b[36;1m",
     white = "\u001b[37m",
-    boldWhite = "\u001b[37m;1m"
+    boldWhite = "\u001b[37;1m"
 }
 
 /++
