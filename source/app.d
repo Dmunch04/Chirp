fa5bee725973a0db08a6f2035e1d67966f930889
Daniel Munch <daniellmunch@gmail.com>
2020-05-28 20:13:45 +0200

    Remove unused code

--- source/app.d
+++ source/app.d
@@ -5,28 +5,9 @@ import dllvm;
 import std.stdio;
 
 // TODO: Fix generated docs
-// TODO: Turns out you don't need to name params in extern functions. oof
-
-struct Lexer
-{
-	ulong a = 0;
-}
-
-void lex(ref Lexer lexer, string source)
-{
-	if (lexer.a == 0)
-	{
-		lexer.a = source.length;
-	}
-}
-
 void main(string[] args)
 {
 	//Program program = new Program(args);
 
 	writeln(dllvm.LLVMVoidType());
-
-	Lexer lexer = Lexer();
-	lexer.lex("aaa");
-	writeln(lexer.a);
 }
